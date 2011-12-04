fe07d1934ecbbf3358abea881c671587b1e79aae
C. Scott Ananian <cscott@cscott.net>
2011-12-04 15:09:32 -0500

    Add script to run 'Chapter 10' WebGL demo in v8-gl standalone.

---
+++ examples/webgl/run-ch10-multitexture.js
@@ -0,0 +1,14 @@
+load('dom.js'); // initial dom-like context
+load('v8glcanvas.js'); // our canvas emulation
+
+load('Common/esShader.js');
+load('Common/esShapes.js');
+load('Common/esTransform.js');
+load('Common/esUtil.js');
+
+document.baseURI = 'Chapter_10/MultiTexture/MultiTexture.js';
+load(document.baseURI);
+
+esContext = main(v8gl_canvas);
+
+if (_intervalFunc) { esContext.gl.mainLoop(_intervalFunc); }
