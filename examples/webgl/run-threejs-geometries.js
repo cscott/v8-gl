2827bc6d636dc41273544618074bc6a68a8755c2
C. Scott Ananian <cscott@cscott.net>
2011-12-08 15:03:50 -0500

    tweak webgl_geometries runner script.

--- examples/webgl/run-threejs-geometries.js
+++ examples/webgl/run-threejs-geometries.js
@@ -5,8 +5,11 @@ load("v8glcanvas.js");
 
 load("three.js/Three.js");
 
-load('three.js/webgl_geometries.js');
+document.baseURI = 'three.js/webgl_geometries.js';
+load(document.baseURI);
 
+v8gl_canvas.width=window.innerWidth=1024;
+v8gl_canvas.height=window.innerHeight=768;
 init(v8gl_canvas);
 
 renderer.context.mainLoop(animate);
